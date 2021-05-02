31b85364bb5d3480f8df41dd38f50db961f6dd09
Quinn T <Quinn.GitHub@gmail.com>
2021-05-02 01:09:21 -0500

    Export type of core module, for easy typing.

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,8 +1,18 @@
 const core = (async()=>{
     const Query = await (await import("supi-core-query")).default();
     return {
-        User: await import("./user")
+        User: new (await import("./user")).default(Query)
     }
 })();
 
 export default core;
+/**
+ * This removes the Promise around a type.
+ * 
+ * `UnwrapPromise<Promise< YOUR_TYPE_HERE >> = YOUR_TYPE_HERE`
+ * 
+ * if `any` cannot go into your type, then it will become `unknown`
+ */
+type UnwrapPromise<T extends Promise<any>> = T extends Promise<infer U> ? U : unknown;
+
+export type Core = UnwrapPromise<typeof import(".").default>
