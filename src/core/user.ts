76c7c724e1e796eeef5559f68d40878511ece63a
Quinn T <Quinn.GitHub@gmail.com>
2021-05-01 23:19:19 -0500

    Sperate User class into 3
    rawUser: raw data to be passed to User class
    User: The actuall user objects
    UserManager: Fetches and stores users.

--- src/core/user.ts
+++ src/core/user.ts
@@ -1,39 +1,64 @@
-import DB from "./db";
-
-class User {
+import { sbQuery, sbDate } from 'supi-core-query';
+import TemplateCoreModule from './template';
+export interface rawUser {
+    /** Uniqu numeric ID */
     ID: number;
+    /** Twitch username */
     Name: string;
+    /** Twitch ID */
     Twitch_ID: number;
-    First_Seen: Date;
-    
-        // should be pretty simple at the start, lets store these properties
-        // twitch id
-        // local id
-        // name
-        // started using (auto set)
-    
-        constructor(ID: number, Name: string, Twitch_ID: number, First_Seen: Date) {
-            this.ID = ID;
-            this.Name = Name;
-            this.Twitch_ID = Twitch_ID;
-            this.First_Seen = First_Seen;
-        }
+    /** Date & time first seen */
+    First_Seen: sbDate;
 }
 
-class UserManager {
-    static Users: User[] = [];
-
-    static get (identifier: number|string|User): User|null {
-        if (identifier instanceof User) return identifier;
-        else if (typeof identifier === "string") {
-            return this.Users.find(user=>user.Name === identifier) ?? null;
-        }
-        else if (typeof identifier === "number") {
-            return this.Users.find(user=>user.ID === identifier) ?? null;
-        } else {
-
-        }
-        return null;
+export class User {
+    /** Uniqu numeric ID */
+    ID: number;
+    /** Twitch username */
+    Name: string;
+    /** Twitch ID */
+    Twitch_ID: number;
+    /** Date & time first seen */
+    First_Seen: sbDate;
+    constructor(data: rawUser) {
+        this.ID = data.ID;
+        this.Name = data.Name;
+        this.Twitch_ID = data.Twitch_ID;
+        this.First_Seen = data.First_Seen;
     }
 }
-export default UserManager;
+
+export default class UserManager extends TemplateCoreModule {
+    static data: User[];
+
+    static async get (identifier: number|string|User): Promise<User|null> {
+        if (identifier instanceof User) {
+            return identifier;
+        }
+        let condition:string;
+        if (typeof identifier === "string") {
+            let candidate = this.data.find(user=>user.Name === identifier);
+            if (candidate) return candidate;
+            condition = "Name = %s"
+        }
+        else if (typeof identifier === "number") {
+            let candidate = this.data.find(user=>user.ID === identifier);
+            if (candidate) return candidate;
+            condition = "ID = %n";
+        }
+        
+        let possible_users: rawUser[]|[rawUser]|[] = await this.Query.getRecordset(rs=>rs
+            .select("*")
+            .from("wb_core", "user")
+            // @ts-ignore
+            .where(condition, identifier)
+            .limit(1)
+        );
+
+        if (possible_users.length < 1) return null;
+
+        let user = new User(possible_users[0])
+        this.data.push(user);
+        return user;
+    }
+}
