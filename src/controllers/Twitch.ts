7d8b0aa4d40b462e579fe38d5241b1c183c31ce4
Quinn T <Quinn.GitHub@gmail.com>
2021-05-02 01:09:02 -0500

    Implement new methods on twitch controller

--- src/controllers/Twitch.ts
+++ src/controllers/Twitch.ts
@@ -1,23 +1,54 @@
 import AbstractController from "./abstract";
 import DankIRC from "dank-twitch-irc";
+import { UserIdentifier } from '../core/user';
+import { Core } from '../core'
 
-export class TwitchController extends AbstractController {
+export default class TwitchController extends AbstractController {
     client: DankIRC.ChatClient;
-    channels: string[];
-    constructor() {
-        super()
-        this.channels = [];
+    get channels (): Set<string> {
+        return this.client.wantedChannels;
+    };
+
+    get isConnected () {
+        return this.client.connected;
+    }
+
+    constructor(core: Core) {
+        super(core)
         this.client = new DankIRC.ChatClient({
             username: "wanductbot",
             password: process.env.TWITCH_OAUTH
         })
-        this.client.on("PRIVMSG", this.handlePrivmsg)
     }
+
+    async initialize () {
+        this.client.on("PRIVMSG", this.handlePrivmsg);
+        await this.connect();
+    }
+
     async connect() {
         this.client.connect();
-
     }
+
     handlePrivmsg (msg: DankIRC.PrivmsgMessage) {
-        
+        console.log("message", { msg: msg.messageText, user: msg.senderUsername, channel: msg.channelName })
+    }
+
+    async dm (identifier: UserIdentifier, message: string) {
+        let user = (await this.core.User.get(identifier))
+        if (!user) throw new Error(`Cannot whisper user identified by '${identifier}' (type ${typeof identifier}), no such user.`);
+        await this.client.whisper(user.Name, message);
+    }
+
+    async join (channel: string) {
+        await this.client.join(channel)
+    }
+
+    async stop () {
+        this.client.close()
+    }
+
+    async part (channel: string) {
+        await this.client.part(channel)
     }
 }
