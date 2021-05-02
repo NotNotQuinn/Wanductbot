0d55f3a44dc954ea65a605891c2ce423dea93499
Quinn T <Quinn.GitHub@gmail.com>
2021-05-02 01:11:04 -0500

    Connect using the twitch controller in start.ts

--- src/start.ts
+++ src/start.ts
@@ -1,6 +1,11 @@
+import AbstractController from './controllers/abstract'
+import { Core } from './core'
+import TwitchController from './controllers/twitch'
 (async()=>{
-    const core = await import("./core");
-    ["twitch"].forEach(platform => {
-        
-    });
+    require("../db-access")
+    const core: Core = await (await import("./core")).default;
+
+    const controller = new TwitchController(core);
+    controller.connect()
+
 })();
