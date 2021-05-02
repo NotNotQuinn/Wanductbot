dfc86201f8d3d74c48ecdbcb35bcd37272a54004
Quinn T <Quinn.GitHub@gmail.com>
2021-05-02 01:08:43 -0500

    Update abstract controller with more methods

--- src/controllers/Abstract.ts
+++ src/controllers/Abstract.ts
@@ -1,7 +1,25 @@
+import { UserIdentifier } from '../core/user'
+import { Core } from '../core'
+
 abstract class AbstractController {
-    // abstract connect(...args: any[]): Promise<void>;
-    // abstract join(channel: string): Promise<void>;
-    // abstract dm(user: string /* XXX user class */, message: string) : Promise<void>;
+    abstract get isConnected(): boolean;
+    abstract get channels(): Set<string>;
+    core: Core;
+    constructor(core: Core) {
+        this.core = core;
+    }
+    /** Connect and start liscening for messages. */
+    abstract connect(...args: any[]): Promise<void>;
+    /** Join a channel. */
+    abstract join(channel: string): Promise<void>;
+    /** Leave a channel. */
+    abstract part(channel: string): Promise<void>;
+    /** Send a private message (if able) */
+    abstract dm(user: UserIdentifier, message: string) : Promise<void>;
+    /** Connect, and start executing commands. */
+    abstract initialize(): Promise<void>;
+    /** Disconnect and stop executing commands. */
+    abstract stop(): Promise<void>;
 }
 
 export default AbstractController;
