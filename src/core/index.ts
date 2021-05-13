fcf01f53792f0ca26d64084a6f70e9eb1ad0be79
Quinn T <Quinn.GitHub@gmail.com>
2021-05-13 17:28:31 -0500

    new: dependancy manager for core index.

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,14 +1,15 @@
 import { sbQuery } from "supi-core-query";
-
+// @ts-ignore
+import ghor from 'ghor';
 import TemplateCoreModule from './template';
 
 import UserManager from './user';
 import Config from './config';
 import CommandManager from './command';
-import Channel from './channel';
+import ChannelManager from './channel';
 import CronManager from './cron';
 
-export default (async (options?: Options<Core>) => {
+export default (async (options?: Options<Core>): Promise<Core> => {
     const { whitelist, blacklist } = options ?? {};
 
     function include(module: keyof Core): boolean {
@@ -25,19 +26,53 @@ export default (async (options?: Options<Core>) => {
         return component;
     }
 
-    if (typeof core !== "object") globalThis["core"] = {};
+    let loaded: Set<string> = new Set();
 
-    // Order here is important. 1. Query 2. Config 3. User 4. Channel 5. Command & Cron
-    if (include("Query")) core.Query = await (await import("supi-core-query")).default();
-    if (include("Config")) core.Config = await loadData(Config);
-    if (include("User")) core.User = await loadData(UserManager);
-    if (include("Channel")) core.Channel = await loadData(Channel);
-    if (include("Command")) core.Command = await loadData(CommandManager);
-    if (include("Cron")) core.Cron = await loadData(CronManager);
+    async function loadDataOnce<T extends typeof TemplateCoreModule>(component: T) {
+        if (loaded.has(component.name)) return component;
+        loaded.add(component.name);
+        return loadData(component);
+    }
+
+    let resolve: (mod: string) => any = ghor({
+        Query: async () => {
+            return await (await import("supi-core-query")).default()
+        },
+        Config: async ({ Query }: { Query: Promise<sbQuery> }) => {
+            
+            Config.registerDeps({ Query: await Query })
+            return await loadDataOnce(Config);
+        },
+        User: async ({ Query }: { Query: Promise<sbQuery> }) => {
+            UserManager.registerDeps({ Query: await Query })
+            return await loadDataOnce(UserManager)
+        },
+        Channel: async ({ Query }: { Query: Promise<sbQuery> }) => {
+            ChannelManager.registerDeps({ Query: await Query })
+            return await loadDataOnce(ChannelManager);
+        },
+        Command: async ({ Channel, Config: Conf, User }: { Channel: Promise<typeof ChannelManager>, Config: Promise<typeof Config>, User: Promise<typeof UserManager> }) => {
+            CommandManager.registerDeps({ Channel: await Channel, User: await User, Config: await Conf  })
+            return await loadDataOnce(CommandManager);
+        },
+        Cron: async ({ Config: Conf }: { Config: Promise<typeof Config> }) => {
+            ChannelManager.registerDeps({ Config: await Conf })
+            return await loadDataOnce(CronManager);
+        },
+    }, console.log);
+
+    return {
+        Query: ((include("Query")) && await resolve("Query")) || undefined,
+        Config: ((include("Config")) && await resolve("Config")) || undefined,
+        User: ((include("User")) && await resolve("User")) || undefined,
+        Channel: ((include("Channel")) && await resolve("Channel")) || undefined,
+        Command: ((include("Command")) && await resolve("Command")) || undefined,
+        Cron: ((include("Cron")) && await resolve("Cron")) || undefined,
+    }
 });
 
 export type Core = Partial<{
-    Channel: typeof Channel;
+    Channel: typeof ChannelManager;
     Config: typeof Config;
     User: typeof UserManager;
     Command: typeof CommandManager;
@@ -48,9 +83,4 @@ export type Core = Partial<{
 export type Options<T> = {
     whitelist?: (keyof T)[];
     blacklist?: (keyof T)[];
-}
-
-// define core as a global object
-declare global {
-    var core: Core;
 }
