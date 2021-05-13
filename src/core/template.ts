fc39fae957f8fcecef3d169dccb4e9d5e2df3421
Quinn T <Quinn.GitHub@gmail.com>
2021-05-13 01:00:38 -0500

    Update module template with module dependency registration

--- src/core/template.ts
+++ src/core/template.ts
@@ -1,6 +1,17 @@
+import { Core } from ".";
+
+type CoreDeps<T extends keyof Core> = {
+    [Key in T]-?: Core[Key];
+};
+
 export default abstract class TemplateCoreModule {
+    static core: Core = {};
     /** Data unique to this module. */
     static data: Array<unknown> | Map<unknown, unknown>;
     /** Loads data on startup. */
     static loadData?: () => Promise<void> = undefined;
+    static registerDeps<T extends keyof Core>(deps: CoreDeps<T>): void {
+        Object.assign(this.core, deps);
+    };
+
 }
